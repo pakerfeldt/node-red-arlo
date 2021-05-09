6dcaa89c2d3f0f9ace771c6420fe1c06176d7033
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2021-05-09 14:34:06 +0200

    1

---
+++ arlo/arlo-config.js
@@ -0,0 +1,35 @@
+'use strict'
+
+module.exports = function (RED) {
+  function ArloConfigNode (config) {
+    this.node = RED.nodes.createNode(this, config)
+    var _this = this
+    this.host = config.host
+    this.port = config.port
+    this.password = this.credentials.password
+    config.password = this.password
+    config.zones = config.zones
+    config.atomicEvents = false
+    config.partitions = config.partitions
+    this.connected = false
+    this.connecting = false
+
+    this.el = new EnvisaLink(config)
+
+    this.users = {}
+
+    this.register = function (node) {
+      _this.log(RED._(node))
+    }
+
+    this.deregister = function (node, done) {
+      _this.log(RED._(node))
+    }
+  }
+
+  RED.nodes.registerType('arlo-config', ArloConfigNode, {
+    credentials: {
+      password: { type: 'password' }
+    }
+  })
+}
